eb826ed9bba9b95053dbf7707c1932ba9c760ded
Josh Turner <joshaturner1@googlemail.com>
2019-01-31 13:59:46 +0000

    Public public publics

---
+++ src/main/java/spe/uoblibraryserver/api/UoBLibrary.java
@@ -0,0 +1,12 @@
+package spe.uoblibraryserver.api;
+
+class UoBLibrary {
+  
+  public static String getOCLC_SYMBOL() {
+    return "BUB";
+  }
+  
+  public static String getREGISTRY_ID() {
+    return "132607";
+  }
+}

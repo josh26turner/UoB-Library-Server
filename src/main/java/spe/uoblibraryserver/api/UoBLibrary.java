ce8e7bd63d6e488a3d26dae97a61996a210c8acf
Josh Turner <joshaturner1@googlemail.com>
2019-02-01 13:06:58 +0000

    Added nonce creation

--- src/main/java/spe/uoblibraryserver/api/UoBLibrary.java
+++ src/main/java/spe/uoblibraryserver/api/UoBLibrary.java
@@ -1,12 +1,27 @@
 package spe.uoblibraryserver.api;
 
+import java.util.Random;
+
 class UoBLibrary {
   
-  public static String getOCLC_SYMBOL() {
+  static String getOCLC_SYMBOL() {
     return "BUB";
   }
   
-  public static String getREGISTRY_ID() {
+  static String getREGISTRY_ID() {
     return "132607";
+  }
+  
+  static String getNonce() {
+    Random random = new Random();
+    
+    int i = random.nextInt(Integer.MAX_VALUE);
+    int j = random.nextInt(Integer.MAX_VALUE);
+    
+    return Integer.toString(i) + j;
+  }
+  
+  static String getPrivateKey() {
+    return "";
   }
 }

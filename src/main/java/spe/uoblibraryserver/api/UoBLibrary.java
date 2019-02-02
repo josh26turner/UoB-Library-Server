ab79b3bc9bcc721f8a9262b2ff7b892a9e1e6667
Josh Turner <joshaturner1@googlemail.com>
2019-02-02 01:23:09 +0000

    XML yay....

--- src/main/java/spe/uoblibraryserver/api/UoBLibrary.java
+++ src/main/java/spe/uoblibraryserver/api/UoBLibrary.java
@@ -1,27 +1,40 @@
 package spe.uoblibraryserver.api;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Random;
 
 class UoBLibrary {
   
-  static String getOCLC_SYMBOL() {
+  static String getOCLCSymbol() {
     return "BUB";
   }
   
-  static String getREGISTRY_ID() {
+  static String getRegistryId() {
     return "132607";
   }
-  
+
   static String getNonce() {
     Random random = new Random();
-    
+
     int i = random.nextInt(Integer.MAX_VALUE);
     int j = random.nextInt(Integer.MAX_VALUE);
-    
+
     return Integer.toString(i) + j;
   }
-  
-  static String getPrivateKey() {
-    return "";
+
+  static String getPrivateKey() throws IOException {
+    FileReader fileReader = new FileReader("src/main/resources/WMSSecretKey.key");
+    BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+    return bufferedReader.readLine();
+  }
+
+  static String getPublicKey() throws IOException {
+    FileReader fileReader = new FileReader("src/main/resources/WMSPublicKey.key");
+    BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+    return bufferedReader.readLine();
   }
 }
